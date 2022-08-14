9272736e37c8b8c1ddc2bf9fb3b62d352a4ca300
shionn <shionn@gmail.com>
2022-08-14 14:52:40 +0200

    connection wifi

--- Tank/src/main.cpp
+++ Tank/src/main.cpp
@@ -1,9 +1,40 @@
-#include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+const char *ssid = "AsusHome";
+const char *password = "aazzeerrttyy";
+
+// https://www.dfrobot.com/blog-994.html
+WiFiServer wifiServer(2300);
+
+void reconnectIfNeed() {
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Connecting");
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(WiFi.status());
+    }
+    Serial.println();
+    Serial.print("IP : ");
+    Serial.print(WiFi.localIP());
+    Serial.print("\tEth : ");
+    Serial.println(WiFi.macAddress());
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  WiFi.disconnect();
+  WiFi.setAutoConnect(true);
+  WiFi.setAutoReconnect(true);
+  WiFi.begin(ssid, password);
+
+  reconnectIfNeed();
+  wifiServer.begin();
 }
 
 void loop() {
+  reconnectIfNeed();
   // put your main code here, to run repeatedly:
 }
